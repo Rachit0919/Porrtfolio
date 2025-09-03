924686e79f25cbb1ce998b3c5dbf40269a284690
Rachit0919 <rachit0919@gmail.com>
2025-09-03 18:34:59 +0530

    Initial commit

---
+++ src/constants/config.ts
@@ -0,0 +1,90 @@
+type TSection = {
+  p: string;
+  h2: string;
+  content?: string;
+};
+
+type TConfig = {
+  html: {
+    title: string;
+    fullName: string;
+    email: string;
+  };
+  hero: {
+    name: string;
+    p: string[];
+  };
+  contact: {
+    form: {
+      name: {
+        span: string;
+        placeholder: string;
+      };
+      email: {
+        span: string;
+        placeholder: string;
+      };
+      message: {
+        span: string;
+        placeholder: string;
+      };
+    };
+  } & TSection;
+  sections: {
+    about: Required<TSection>;
+    experience: TSection;
+    feedbacks: TSection;
+    works: Required<TSection>;
+  };
+};
+
+export const config: TConfig = {
+  html: {
+    title: "Rachit Joshi — 3D Portfolio",
+    fullName: "John Doe",
+    email: "johndoe@mail.com",
+  },
+  hero: {
+    name: "Rachit Joshi",
+    p: ["I break things until they scale,", "Crafting scalable backends & interactive frontends"],
+  },
+  contact: {
+    p: "Get in touch",
+    h2: "Contact.",
+    form: {
+      name: {
+        span: "Your Name",
+        placeholder: "What's your name?",
+      },
+      email: { span: "Your Email", placeholder: "What's your email?" },
+      message: {
+        span: "Your Message",
+        placeholder: "What do you want to say?",
+      },
+    },
+  },
+  sections: {
+    about: {
+      p: "Introduction",
+      h2: "Overview.",
+      content: `I’m a software developer skilled in JavaScript and TypeScript, with expertise in React, Node.js, and MongoDB. I specialise in building scalable backends with MongoDB’s aggregation pipelines and secure JWT-based authentication, while creating seamless frontends with React. I’m a quick learner who enjoys collaboration, turning complex ideas into simple, impactful, and user-friendly solutions. Let’s bring your vision to life!`,
+    },
+    experience: {
+      p: "What I have done so far",
+      h2: "Work Experience.",
+    },
+    feedbacks: {
+      p: "What others say",
+      h2: "Testimonials.",
+    },
+    works: {
+      p: "My work",
+      h2: "Projects.",
+      content: `Following projects showcases my skills and experience through
+    real-world examples of my work. Each project is briefly described with
+    links to code repositories and live demos in it. It reflects my
+    ability to solve complex problems, work with different technologies,
+    and manage projects effectively.`,
+    },
+  },
+};
